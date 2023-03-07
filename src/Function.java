a729eeb07ab30cc3ec182f78d666f2ab5ab2ecda
Pisethsek <pisethsek9899@gmail.com>
2023-03-07 13:18:43 +0700

    first commit

---
+++ src/Function.java
@@ -0,0 +1,20 @@
+public class Function {
+
+    public static float sum(float a, float b){
+        return 1;
+    }
+
+    public static float sub(float a, float b){
+        return 1;
+    }
+
+    public static float mul(float a, float b){
+        return 1;
+    }
+
+    public static float divide(float a, float b){
+        return 1;
+    }
+
+
+}
